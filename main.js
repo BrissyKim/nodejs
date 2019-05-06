e8bb6f81f8ce1288fdc0d83d4cd71d557a507c46
SeyoungKim <ksy4961@gmail.com>
2019-05-06 23:01:45 +1000

    add delete function

--- main.js
+++ main.js
@@ -59,7 +59,13 @@ var app = http.createServer(function (request, response) {
         fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
           var title = queryData.id;
           var list = templateList(fileList);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a> \
+            <form action="delete_process" method = "POST" onsubmit>
+              <input type="hidden" name = "id" value="${title}">
+              <input type="submit" value ="delete">
+            </form>`
+          );
           response.writeHead(200);
           response.end(template);
         });
@@ -142,6 +148,21 @@ var app = http.createServer(function (request, response) {
           });
           response.end(); // redirection 
         });
+      });
+    });
+  } else if (pathname === '/delete_process') {
+    var body = '';
+    request.on('data', function (data) {
+      body = body + data;
+    });
+    request.on('end', function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      fs.unlink(`data/${id}`, function (error) {
+        response.writeHead(302, {
+          Location: `/`
+        });
+        response.end();
       });
     });
   } else {
